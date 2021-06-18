e82da50d70d2122fc215004c261f756fce7f9003
Raul Metsma <raul@metsma.ee>
2021-06-18 10:46:08 +0300

    Show about dialog
    WE2-465
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/mac/shared.hpp
+++ src/mac/shared.hpp
@@ -22,10 +22,35 @@
 
 #pragma once
 
-static NSString* WebEidApp = @"ee.ria.web-eid-safari";
-static NSString* WebEidExtension = @"ee.ria.web-eid-safari.web-eid-safari-extension";
-static NSString* WebEidShared = @"ee.ria.web-eid-safari.shared";
+static NSString* WebEidApp = @"eu.web-eid.web-eid-safari";
+static NSString* WebEidExtension = @"eu.web-eid.web-eid-safari.web-eid-safari-extension";
+static NSString* WebEidShared = @"eu.web-eid.web-eid-safari.shared";
+static NSString* WebEidStarting = @"eu.web-eid.web-eid-safari.starting";
 
 #if __MAC_OS_X_VERSION_MIN_REQUIRED < 110000
 NSString* const SFExtensionMessageKey = @"message";
 #endif
+
+static NSUserDefaults* getUserDefaults()
+{
+    return [[NSUserDefaults alloc] initWithSuiteName:WebEidShared];
+}
+
+static void setValue(NSString* key, id value)
+{
+    NSUserDefaults* defaults = getUserDefaults();
+    [defaults setObject:value forKey:key];
+    [defaults synchronize];
+}
+
+static id takeValue(NSString* key)
+{
+    NSUserDefaults* defaults = getUserDefaults();
+    NSDictionary* value = [defaults objectForKey:key];
+    if (value == nil) {
+        return value;
+    }
+    [defaults removeObjectForKey:key];
+    [defaults synchronize];
+    return value;
+}
