0a57aed73495f0aa5b7ae83e8e824493e11c4f8f
Raul Metsma <raul@metsma.ee>
2021-10-25 15:29:56 +0300

    Quit application when Safari settings are presented
    WE2-597
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/mac/main.mm
+++ src/mac/main.mm
@@ -163,6 +163,7 @@ public:
     {
         [SFSafariApplication showPreferencesForExtensionWithIdentifier:WebEidExtension
                                                      completionHandler:nil];
+        quit();
     }
 };
 
