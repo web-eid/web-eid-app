63d3826a4f375f6f37b7ff9de1eb0cab7c6075b0
Mart Somermaa <mrts@users.noreply.github.com>
2022-06-14 22:33:02 +0300

    update(translations): update Croatian translations

--- src/ui/translations/hr.ts
+++ src/ui/translations/hr.ts
@@ -5,37 +5,37 @@
     <name>Application</name>
     <message>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Datoteka</translation>
     </message>
     <message>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;O programu</translation>
     </message>
 </context>
 <context>
     <name>CertificateButton</name>
     <message>
         <source>%1 Issuer: %2 Valid: %3 to %4</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Izdavatelj: %2 Vrijedi: %3 do %4</translation>
     </message>
 </context>
 <context>
     <name>CertificateWidget</name>
     <message>
         <source>Pin locked</source>
-        <translation type="unfinished"></translation>
+        <translation>Zaključano</translation>
     </message>
     <message>
         <source> (Not effective)</source>
-        <translation type="unfinished"></translation>
+        <translation> (Nije valjano)</translation>
     </message>
     <message>
         <source> (Expired)</source>
-        <translation type="unfinished"></translation>
+        <translation> (Isteklo)</translation>
     </message>
     <message>
         <source>&lt;b&gt;%1&lt;/b&gt;&lt;br /&gt;Issuer: %2&lt;br /&gt;Valid: %3 to %4%5</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt;&lt;br /&gt;Izdavatelj: %2&lt;br /&gt;Vrijedi: %3 do %4%5</translation>
     </message>
 </context>
 <context>
@@ -50,11 +50,11 @@
     </message>
     <message>
         <source>Waiting for card</source>
-        <translation type="unfinished"></translation>
+        <translation>Čekanje na iskaznicu</translation>
     </message>
     <message>
         <source>Operation failed. More detailed information can be found in the log files.</source>
-        <translation type="unfinished"></translation>
+        <translation>Radnja nije uspjela. Detaljnije informacije mogu se pronaći u datoteci zapisnika radnji.</translation>
     </message>
     <message>
         <source>Get help here: &lt;a href=&quot;https://www.id.ee&quot;&gt;id.ee&lt;/a&gt;</source>
@@ -62,215 +62,215 @@
     </message>
     <message>
         <source>Select a certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>Odabir vjerodajnice</translation>
     </message>
     <message>
         <source>By choosing the certificate, I agree to the transfer of my name and personal identification code to the service provider.</source>
-        <translation type="unfinished"></translation>
+        <translation>Odabirom vjerodajnice, pristajem na prijenos mog imena i osobnog identifikacijskog broja davatelju usluge.</translation>
     </message>
     <message>
         <source>By authenticating, I agree to the transfer of my name and personal identification code to the service provider.</source>
-        <translation type="unfinished"></translation>
+        <translation>Pristajem na prijenos mog imena i osobnog identifikacijskog broja davatelju usluge.</translation>
     </message>
     <message>
         <source>Select another certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>Odabir druge vjerodajnice</translation>
     </message>
     <message>
         <source>Enter PIN1 for authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>Unos PIN1 za identifikaciju</translation>
     </message>
     <message>
         <source>About Web eID</source>
-        <translation type="unfinished"></translation>
+        <translation>O Web eID</translation>
     </message>
     <message>
         <source>Web eID extension is disabled. The extension must be enabled in the web browser to authenticate and sign with an ID-card.</source>
-        <translation type="unfinished"></translation>
+        <translation>Web eID proširenje je onemogućeno. Proširenje mora biti omogućeno u web pregledniku za identifikaciju i potpisivanje sa osobnom iskaznicom.</translation>
     </message>
     <message>
         <source>&lt;b&gt;This is a companion application to the Web eID browser extension&lt;/b&gt; and cannot be used independently. To uninstall the extension, remove this application.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Ovo je prateća aplikacija Web eID proširenju preglednika&lt;/b&gt; i ne može se koristiti neovisno. Da bi uklonili proširenje, uklonite aplikaciju.</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Prekid</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Pomoć</translation>
     </message>
     <message>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>Potvrda</translation>
     </message>
     <message>
         <source>Version: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Inačica: %1</translation>
     </message>
     <message>
         <source>https://www.id.ee/en/article/how-to-check-that-your-id-card-reader-is-working/</source>
-        <translation type="unfinished"></translation>
+        <translation>https://www.id.ee/en/article/how-to-check-that-your-id-card-reader-is-working/</translation>
     </message>
     <message>
         <source>Time remaining: &lt;b&gt;%1&lt;/b&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Preostalo vremena: &lt;b&gt;%1&lt;/b&gt;</translation>
     </message>
     <message>
         <source>Open Safari settings...</source>
-        <translation type="unfinished"></translation>
+        <translation>Otvoranje Safari postavki...</translation>
     </message>
     <message>
         <source>Operation failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Radnja nije uspjela</translation>
     </message>
     <message>
         <source>Signing</source>
-        <translation type="unfinished"></translation>
+        <translation>Potpisivanje</translation>
     </message>
     <message>
         <source>By signing, I agree to the transfer of my name and personal identification code to the service provider.</source>
-        <translation type="unfinished"></translation>
+        <translation>Potpisivanjem pristajem na prijenos mog imena i osobnog identifikacijskog broja davatelju usluge.</translation>
     </message>
     <message>
         <source>Enter PIN2 for signing</source>
-        <translation type="unfinished"></translation>
+        <translation>Unos PIN2 za potpisivanje</translation>
     </message>
     <message>
         <source>The certificate of the ID card in the reader does not match the originally submitted certificate. Please insert the original ID card.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vjerodajnica osobne iskaznice u čitaču ne odgovara izvorno poslanoj vjerodajnici. Molimo, umetnite izvornu osobnu iskaznicu u čitač.</translation>
     </message>
     <message>
         <source>Incorrect PIN.</source>
-        <translation type="unfinished"></translation>
+        <translation>Neispravan PIN.</translation>
     </message>
     <message numerus="yes">
         <source>Incorrect PIN, %n attempts left.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Neispravan PIN, preostalo %n pokušaj/a.</numerusform>
+            <numerusform>Neispravan PIN, preostalo %n pokušaj/a.</numerusform>
+            <numerusform>Neispravan PIN, preostalo %n pokušaj/a.</numerusform>
         </translation>
     </message>
     <message>
         <source>Invalid PIN length</source>
-        <translation type="unfinished"></translation>
+        <translation>Neispravna duljina PIN-a</translation>
     </message>
     <message>
         <source>PinPad timed out waiting for customer interaction.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vrijeme čekanja na unos je isteklo.</translation>
     </message>
     <message>
         <source>PIN entry cancelled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Unos PIN-a je otkazan.</translation>
     </message>
     <message>
         <source>Try again</source>
-        <translation type="unfinished"></translation>
+        <translation>Ponovni pokušaj</translation>
     </message>
     <message>
         <source>Please enter %1 in PinPad reader</source>
-        <translation type="unfinished"></translation>
+        <translation>Molimo unos %1 u čitač</translation>
     </message>
     <message>
         <source>PIN1 for authentication</source>
-        <translation type="unfinished"></translation>
+        <translation>PIN1 za identifikaciju</translation>
     </message>
     <message>
         <source>PIN2 for signing</source>
-        <translation type="unfinished"></translation>
+        <translation>PIN2 za potpisivanje</translation>
     </message>
     <message numerus="yes">
         <source>The PIN has been entered incorrectly at least once. %n attempts left.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>PIN je unesen neispravno barem jednom. Preostalo %n pokušaj/a.</numerusform>
+            <numerusform>PIN je unesen neispravno više puta. Preostalo %n pokušaj/a.</numerusform>
+            <numerusform>PIN je unesen neispravno više puta. Preostalo %n pokušaj/a.</numerusform>
         </translation>
     </message>
     <message>
         <source>PIN is locked. Unblock and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>PIN je zaključan. Odblokirajte ga i pokušajte ponovo.</translation>
     </message>
     <message>
         <source>The smart card service required to use the ID-card is not running. Please start the smart card service and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Servis za rad s osobnom iskaznicom nije pokrenut. Molimo, pokrenite servis i pokušajte ponovo.</translation>
     </message>
     <message>
         <source>Launch the Smart Card service</source>
-        <translation type="unfinished"></translation>
+        <translation>Pokrenite servis za osobnu iskaznicu</translation>
     </message>
     <message>
         <source>&lt;b&gt;Card reader not connected.&lt;/b&gt; Please connect the card reader to the computer.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Čitač kartica nije povezan.&lt;/b&gt; Molimo, povežite čitač kartica sa računalom.</translation>
     </message>
     <message>
         <source>Connect the card reader</source>
-        <translation type="unfinished"></translation>
+        <translation>Povezivanje čitača kartica</translation>
     </message>
     <message>
         <source>&lt;b&gt;ID-card not found.&lt;/b&gt; Please insert the ID-card into the reader.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Osobna iskaznica nije pronađena.&lt;/b&gt; Moimo, umetnite osobnu iskaznicu u čitač.</translation>
     </message>
     <message>
         <source>Insert the ID-card</source>
-        <translation type="unfinished"></translation>
+        <translation>Umetanje osobne iskaznice</translation>
     </message>
     <message>
         <source>The ID-card was removed from the reader. Please insert the ID-card into the reader.</source>
-        <translation type="unfinished"></translation>
+        <translation>Osobna iskaznica je uklonjena iz čitača. Molimo, umetnite osobnu iskaznicu u čitač.</translation>
     </message>
     <message>
         <source>Operation failed. Make sure that the ID-card and the card reader are connected correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Radnja nije uspjela. Provjerite jesu li osobna iskaznica i čitač pravilo povezani.</translation>
     </message>
     <message>
         <source>Check the ID-card and the reader connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Provjerite vezu osobne iskaznice i čitača</translation>
     </message>
     <message>
         <source>Connection to the ID-card or reader failed. Make sure that the ID-card and the card reader are connected correctly.</source>
-        <translation type="unfinished"></translation>
+        <translation>Veza sa osobnom iskaznicom ili čitačem kartica nije uspjela. Provjerite jesu li osobna iskaznica i čitač kartica ispravno povezani.</translation>
     </message>
     <message>
         <source>The desired operation cannot be performed with the inserted ID-card. Make sure that the ID-card is supported by the Web eID application.</source>
-        <translation type="unfinished"></translation>
+        <translation>Željena radnja ne može se izvesti pomoću umetnute osobne iskaznice. Provjerite je li osobna iskaznica podržana od strane Web eID aplikacije.</translation>
     </message>
     <message>
         <source>Operation not supported</source>
-        <translation type="unfinished"></translation>
+        <translation>Radnja nije podržana</translation>
     </message>
     <message>
         <source>Error communicating with the card.</source>
-        <translation type="unfinished"></translation>
+        <translation>Greška u komunikaciji s iskaznicom.</translation>
     </message>
     <message>
         <source>Card driver error. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Greška upravljačkog programa. Pokušajte ponovo.</translation>
     </message>
     <message>
         <source>Card driver error</source>
-        <translation type="unfinished"></translation>
+        <translation>Greška upravljačkog programa</translation>
     </message>
     <message>
         <source>An error occurred in the Smart Card service required to use the ID-card. Make sure that the ID-card and the card reader are connected correctly or relaunch the Smart Card service.</source>
-        <translation type="unfinished"></translation>
+        <translation>Došlo je do greške u radu servisa potrebnim za rad s osobnom iskaznicom. Provjerite jesu li osobna iskaznica i čitač kartica ispravno povezani ili ponovo pokrenite servis za rad s osobnom iskaznicom.</translation>
     </message>
     <message>
         <source>The card in the reader is not supported. Make sure that the entered ID-card is supported by the Web eID application.</source>
-        <translation type="unfinished"></translation>
+        <translation>Osobna iskaznica u čitaču nije podržana. Provjerite je li umetnuta osobna iskaznica podržana od strane Web eID aplikacije.</translation>
     </message>
     <message>
         <source>The certificates of the ID-card have expired. Valid certificates are required for the electronic use of the ID-card.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vjerodajnica na osobnoj iskaznici je istekla. Valjana osobna iskaznica je neophodna za elektroničku uporabu.</translation>
     </message>
     <message>
         <source>Operation failed. Make sure that the driver of the corresponding card reader is used. Read more &lt;a href=&quot;https://www.id.ee/en/article/using-pinpad-card-reader-drivers/&quot;&gt;here&lt;/a&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Radnja nije uspjela. Provjerite koristi li se ispravan pokretački program za osobnu iskaznicu. Pročitajte više o tome na &lt;a href=&quot;https://www.id.ee/en/article/using-pinpad-card-reader-drivers/&quot;&gt;ovdje&lt;/a&gt;.</translation>
     </message>
     <message>
         <source>Unknown error</source>
-        <translation type="unfinished"></translation>
+        <translation>Nepoznata greška</translation>
     </message>
 </context>
 </TS>
