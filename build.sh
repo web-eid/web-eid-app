b69da238def0a01019faf280f3bf1e45bf1d0645
Raul Metsma <raul@metsma.ee>
2023-06-12 13:39:27 +0300

    Use official macOS QT binaries
    WE2-796
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- build.sh
+++ build.sh
@@ -3,19 +3,5 @@
 set -e
 set -u
 
-PROJECT_ROOT="$(cd "$( dirname "$0" )"; pwd)"
-
-# Verify that repository has been cloned with submodules
-
-cd "$PROJECT_ROOT/lib/libelectronic-id"
-
-[[ -e README.md ]] || { echo "FAIL: libelectronic-id submodule directory empty, did you 'git clone --recursive'?"; exit 1; }
-
-# Build everything
-
-cd "$PROJECT_ROOT"
-
-BUILD_TYPE=RelWithDebInfo
-
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -B build -S .
-cmake --build build --config $BUILD_TYPE # -- VERBOSE=1
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -B build -S .
+cmake --build build # -- VERBOSE=1
