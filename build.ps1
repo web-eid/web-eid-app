75b75de4354771671f0a9eb85065822020630703
Raul Metsma <raul@metsma.ee>
2023-06-12 13:39:27 +0300

    Use official macOS QT binaries
    WE2-796
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- build.ps1
+++ build.ps1
@@ -1,12 +1,8 @@
 param(
   [string]$cmake = "C:\Program Files\CMake\bin\cmake.exe",
   [string]$vcpkgroot = "C:\vcpkg",
-  [string]$qtdir = "C:\Qt\5.12.7\msvc2017_64"
+  [string]$qtdir = "C:\Qt\6.5.2\msvc2019_64"
 )
 
-$PROJECT_ROOT = split-path -parent $MyInvocation.MyCommand.Definition
-
-Push-Location -Path "$PROJECT_ROOT\build"
-& $cmake -A x64 "-DCMAKE_TOOLCHAIN_FILE=$vcpkgroot\scripts\buildsystems\vcpkg.cmake" "-DQt5_DIR=$qtdir/lib/cmake/Qt5" ..
-& $cmake --build .
-Pop-Location
+& $cmake -A x64 "-DCMAKE_TOOLCHAIN_FILE=$vcpkgroot\scripts\buildsystems\vcpkg.cmake" "-DQt6_DIR=$qtdir" -S . -B build
+& $cmake --build build
