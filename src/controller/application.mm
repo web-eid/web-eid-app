c93134cb70eee1ec1433a65309c81d6c6a1c325f
Raul Metsma <raul@metsma.ee>
2024-01-26 13:43:23 +0200

    Increase version number and copyright year
    WE2-846
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- src/controller/application.mm
+++ src/controller/application.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2023 The Web eID Project
+ * Copyright (c) 2022-2024 The Web eID Project
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
